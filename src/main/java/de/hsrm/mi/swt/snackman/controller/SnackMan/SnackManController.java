713c184daaf32363db24a7fe859e7f791a9bcfef
Dominik <dominik.rundau@student.hs-rm.de>
2024-12-04 11:38:02 +0100

    #104-Backend-Kalorien zählen und MAXKALORIEN Benachrichtigung

---
+++ src/main/java/de/hsrm/mi/swt/snackman/controller/SnackMan/SnackManController.java
@@ -0,0 +1,50 @@
+package de.hsrm.mi.swt.snackman.controller.SnackMan;
+
+import de.hsrm.mi.swt.snackman.controller.PlayerMovement.SnackManUpdateDTO;
+import de.hsrm.mi.swt.snackman.entities.mobileObjects.eatingMobs.SnackMan;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SnackManController {
+
+
+    @Autowired
+    private SnackMan snackman;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+
+    @PostConstruct
+    public void initListener(){
+        snackman.addPropertyChangeListener(event -> {
+            if ( "currentCalories".equals(event.getPropertyName()) ){
+                int newCalories = (int) event.getNewValue();
+                String message = newCalories == snackman.getMAXCALORIES() ? "Maximum calories reached!" : "";
+                SnackManUpdateDTO update = new SnackManUpdateDTO(newCalories, message);
+
+                messagingTemplate.convertAndSend("/topic/update", update);
+            }
+
+        });
+    }
+
+
+/*    @GetMapping("/snackman/calories")
+    public void getSnackManCalories(){
+        int calories = snackman.getCurrentCalories();
+        String massage = calories == snackman.getMAXCALORIES()? "Maximale Kalorien erreicht!" : "";
+        SnackManUpdateDTO update = new SnackManUpdateDTO(calories, massage);
+        messagingTemplate.convertAndSend("/topic/update", update);
+    }
+
+ */
+
+
+
+
+
+}
