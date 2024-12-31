202f0850b67863880b5133361cbb79364e36ad99
Vivien Weber <vivien.weber@student.hs-rm.de>
2024-12-31 12:39:34 +0100

    feat: try to add sound in chicken

--- frontend/src/renderer/GameObjectRenderer.ts
+++ frontend/src/renderer/GameObjectRenderer.ts
@@ -1,22 +1,22 @@
 import * as THREE from 'three'
-import {SnackType} from "@/stores/Snack/ISnackDTD";
-import {ChickenThickness} from "@/stores/Chicken/IChickenDTD";
+import {SnackType} from '@/stores/Snack/ISnackDTD'
+import {ChickenThickness} from '@/stores/Chicken/IChickenDTD'
 
 /**
  * for creating the objects in the map
  * objects are rendered by the GameMapRenderer.ts
  */
 export const GameObjectRenderer = () => {
-
   const GROUNDSIZE = 1000
+  const listener = new THREE.AudioListener()
 
   const createSnackOnFloor = (
     xPosition: number,
     zPosition: number,
     sideLength: number,
-    type: SnackType
+    type: SnackType,
   ) => {
-    let color = 'blue';
+    let color = 'blue'
 
     if (type == SnackType.STRAWBERRY) {
       color = 'purple'
@@ -31,8 +31,12 @@ export const GameObjectRenderer = () => {
     // TODO add correct snack-material-design
     const SNACK_WIDTH_AND_DEPTH = sideLength / 3
     const SNACK_HEIGHT = 1
-    const snackMaterial = new THREE.MeshStandardMaterial({color: color})
-    const snackGeometry = new THREE.BoxGeometry(SNACK_WIDTH_AND_DEPTH, SNACK_HEIGHT, SNACK_WIDTH_AND_DEPTH)
+    const snackMaterial = new THREE.MeshStandardMaterial({ color: color })
+    const snackGeometry = new THREE.BoxGeometry(
+      SNACK_WIDTH_AND_DEPTH,
+      SNACK_HEIGHT,
+      SNACK_WIDTH_AND_DEPTH,
+    )
     const snack = new THREE.Mesh(snackGeometry, snackMaterial)
 
     snack.position.set(xPosition, 0, zPosition)
@@ -44,21 +48,43 @@ export const GameObjectRenderer = () => {
     xPosition: number,
     zPosition: number,
     sideLength: number,
-    thickness: ChickenThickness
+    thickness: ChickenThickness,
   ) => {
-    let color = 'black';
+    const color = 'black'
     const CHICKEN_WIDTH_AND_DEPTH = sideLength / 2
     const CHICKEN_HEIGHT = 15
-    const scale = ChickenThickness[thickness as unknown as keyof typeof ChickenThickness]
+    const scale =
+      ChickenThickness[thickness as unknown as keyof typeof ChickenThickness]
 
     //@TODO add correct chicken-material-design
-    const chickenMaterial = new THREE.MeshStandardMaterial({color: color})
-    const chickenGeometry = new THREE.BoxGeometry(CHICKEN_WIDTH_AND_DEPTH * scale, CHICKEN_HEIGHT, CHICKEN_WIDTH_AND_DEPTH * scale)
+    const chickenMaterial = new THREE.MeshStandardMaterial({ color: color })
+    const chickenGeometry = new THREE.BoxGeometry(
+      CHICKEN_WIDTH_AND_DEPTH * scale,
+      CHICKEN_HEIGHT,
+      CHICKEN_WIDTH_AND_DEPTH * scale,
+    )
     const chicken = new THREE.Mesh(chickenGeometry, chickenMaterial)
     chicken.castShadow = true
     chicken.receiveShadow = true
 
     chicken.position.set(xPosition, 0, zPosition)
+
+    // Add sound to the chicken
+    const listener = new THREE.AudioListener()
+    const sound = new THREE.PositionalAudio(listener)
+    const audioLoader = new THREE.AudioLoader()
+    audioLoader.load(
+      'frontend/src/assets/sounds/chicken_noises.mp3',
+      buffer => {
+        sound.setBuffer(buffer)
+        sound.setRefDistance(100)
+        sound.setLoop(true)
+        sound.setVolume(50.0)
+      },
+    )
+
+    chicken.add(sound)
+    sound.play()
 
     return chicken
   }
@@ -70,8 +96,12 @@ export const GameObjectRenderer = () => {
   ) => {
     // TODO squareHeight is set for seeing it actually in game
     const squareHeight = 0.1
-    const squareMaterial = new THREE.MeshStandardMaterial({color: 'green'})
-    const squareGeometry = new THREE.BoxGeometry(sideLength, squareHeight, sideLength)
+    const squareMaterial = new THREE.MeshStandardMaterial({ color: 'green' })
+    const squareGeometry = new THREE.BoxGeometry(
+      sideLength,
+      squareHeight,
+      sideLength,
+    )
     const square = new THREE.Mesh(squareGeometry, squareMaterial)
 
     // Position the square
@@ -83,7 +113,7 @@ export const GameObjectRenderer = () => {
   const createGround = () => {
     // ground setup
     const groundGeometry = new THREE.PlaneGeometry(GROUNDSIZE, GROUNDSIZE)
-    const groundMaterial = new THREE.MeshMatcapMaterial({color: 'lightgrey'})
+    const groundMaterial = new THREE.MeshMatcapMaterial({ color: 'lightgrey' })
     const ground = new THREE.Mesh(groundGeometry, groundMaterial)
     ground.castShadow = true
     ground.receiveShadow = true
@@ -104,7 +134,7 @@ export const GameObjectRenderer = () => {
     sideLength: number,
   ) => {
     // TODO add correct wall-material-design!!
-    const wallMaterial = new THREE.MeshStandardMaterial({color: 'orange'})
+    const wallMaterial = new THREE.MeshStandardMaterial({ color: 'orange' })
     const wallGeometry = new THREE.BoxGeometry(sideLength, height, sideLength)
     const wall = new THREE.Mesh(wallGeometry, wallMaterial)
 
@@ -114,5 +144,11 @@ export const GameObjectRenderer = () => {
     return wall
   }
 
-  return {createSnackOnFloor, createChickenOnFloor, createFloorSquare, createGround, createWall}
+  return {
+    createSnackOnFloor,
+    createChickenOnFloor,
+    createFloorSquare,
+    createGround,
+    createWall,
+  }
 }
