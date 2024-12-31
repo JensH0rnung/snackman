ae38bd2ef51cc85b66ea6658497e421aa005a63e
Vivien Weber <vivien.weber@student.hs-rm.de>
2024-12-31 15:36:34 +0100

    fix: fine-tune chicken sound

--- frontend/src/renderer/GameObjectRenderer.ts
+++ frontend/src/renderer/GameObjectRenderer.ts
@@ -74,11 +74,10 @@ export const GameObjectRenderer = () => {
     const audioLoader = new THREE.AudioLoader()
     audioLoader.load('/src/assets/sounds/chicken_noises.ogg', buffer => {
       sound.setBuffer(buffer)
-      sound.setRefDistance(3) // maximum volume at x units of distance
-      sound.setMaxDistance(10)
+      sound.setRefDistance(5) // maximum volume at x units of distance
+      sound.setMaxDistance(12) // max distance
       sound.setRolloffFactor(1) // how quickly the volume decreases with increasing distance
       sound.setDistanceModel('linear') // decrease in volume (linear is a good choice for games)
-      sound.setDirectionalCone(180, 230, 0.1)
       sound.setLoop(true)
       sound.setVolume(0.5)
       sound.play()
